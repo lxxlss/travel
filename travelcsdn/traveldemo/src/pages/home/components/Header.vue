3f739108b2e71074b592313a04459c76f6cc9650
lx <2940367526@qq.com>
2019-07-25 11:01:57 +0800

    per

---
+++ travelcsdn/traveldemo/src/pages/home/components/Header.vue
@@ -0,0 +1,58 @@
+<template>
+	<div class="header">
+	  <div class="header-left">
+	  	<span class="iconfont">&#xe6b7;</span>
+	  </div>
+	  <div class="header-input">
+	  	<span class="iconfont">&#xe633;</span>
+	  	输入城市/景点/游玩主题
+	  </div>
+	  <!--点击跳转city页面-->
+	  
+	  <div class="header-right">
+	  	<router-link to='/city'>
+	  	{{this.city}}
+	  	<span class="iconfont">&#xe600;</span>
+	   </router-link>
+	  </div>
+	  
+	</div>
+</template>
+
+<script>
+	 export default {
+	 	name:'HomeHeader',
+	 	props: {
+	 	   city: String
+	 	}
+	 }
+</script>
+
+<style lang="stylus" scoped>
+ @import '~styles/varibles.styl'
+/*1rem = html font-size =50px (86/100)*/
+  .header
+    width:100%
+    display:flex
+    height:$headerHeight
+    line-height:$headerHeight
+    background:$bgColor
+    color:#fff
+    .header-left
+       width:8%
+       float:left
+       color:#fff
+    .header-right
+       width:17%
+       float:right
+       text-align:center
+    .header-input
+       padding-left:0.1rem
+       flex:1
+       background:#fff
+       height:0.6rem
+       margin-top:0.1rem
+       border-radius:0.1rem
+       color:#ccc
+       line-height:0.6rem
+</style>
---
+++ travelcsdn/traveldemo/src/pages/home/components/swipertest.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+	<swiper :options="swiperOption"  ref="mySwiper" @someSwiperEvent="callback">
+    <!-- slides -->
+    <swiper-slide>
+    	<img  class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1802/e3/62ce7362ca051d02.jpg_640x200_6db551b7.jpg"/>
+    </swiper-slide>
+    <swiper-slide>
+    	<img  class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1801/93/ce59d182aca07102.jpg_640x200_ba03d44c.jpg"	/>
+    </swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+    <div class="swiper-button-prev" slot="button-prev"></div>
+    <div class="swiper-button-next" slot="button-next"></div>
+    <div class="swiper-scrollbar"   slot="scrollbar"></div>
+  </swiper>
+	</div>
+</template>
+
+<script>
+	export default{
+		name:'SwiperTest'
+	}
+</script>
+
+<style lang="stylus" scoped>
+    
+	  .swiper-img
+	    width:100%
+</style>
