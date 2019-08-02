e95a59b47abd5d64e0a541d71d96a5a42705762c
lx <2940367526@qq.com>
2019-08-02 11:36:47 +0800

    画廊组件

---
+++ travelcsdn/traveldemo/src/common/fade/FadeAnimation.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'FadeAnimation'
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ travelcsdn/traveldemo/src/pages/detail/Detail.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="detail">
+    <detail-banner></detail-banner>
+	</div>
+</template>
+
+<script>
+import DetailBanner from './components/Banner'
+export default {
+        name:'Detail',
+        components:{
+          DetailBanner: DetailBanner
+         }
+	 }
+</script>
+
+<style lang="stylus" scoped>
+ @import '~styles/varibles.styl'
+/*1rem = html font-size =50px (86/100)*/
+  
+</style>
